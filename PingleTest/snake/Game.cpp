2ef38570c4ef1f3334fa0d0d5cb1f513862b6fed
sahlet <sahlet82@gmail.com>
2020-04-30 17:12:30 +0200

    printFrame

--- PingleTest/snake/Game.cpp
+++ PingleTest/snake/Game.cpp
@@ -1,16 +1,35 @@
-#include <sstream>
+﻿#include <sstream>
 #include <iostream>
 #include "Game.h"
 #include "ConsoleHacks.h"
 
-const int MAX_X = 30, MAX_Y = 30;
+const int MAX_X = 50, MAX_Y = 20;
 const int frameOriginX = 4, frameOriginY = 2;
 const int bufferOriginX = frameOriginX + 1, bufferOriginY = frameOriginY + 1;
 
 void printingFrame()
 {
 	console_hacks::setCursorPosition(frameOriginX, frameOriginY);
-	std::cout << '';
+	std::cout << "\xc9";
+	for (size_t i = 0; i < MAX_X; i++)
+	{
+		std::cout << "\xcd";
+	}
+	std::cout << "\xbb";
+	
+	for (size_t i = 0; i < MAX_Y; i++)
+	{
+		console_hacks::setCursorPosition(frameOriginX, frameOriginY + i + 1);
+		std::cout << "\xba" << std::string(MAX_X, ' ') << "\xba";
+	}
+	
+	console_hacks::setCursorPosition(frameOriginX, frameOriginY + MAX_Y + 1);
+	std::cout << "\xc8";
+	for (size_t i = 0; i < MAX_X; i++)
+	{
+		std::cout << "\xcd";
+	}
+	std::cout << "\xbc";
 }
 
 void print(int score, char prevBuffer[MAX_X][MAX_Y], char buffer[MAX_X][MAX_Y])
@@ -30,7 +49,7 @@ void print(int score, char prevBuffer[MAX_X][MAX_Y], char buffer[MAX_X][MAX_Y])
 		console_hacks::setCursorPosition(i, 0);
 	}
 
-	console_hacks::setCursorPosition();
+	//console_hacks::setCursorPosition();
 }
 
 int play(StateProcessor& stateProcessor)
